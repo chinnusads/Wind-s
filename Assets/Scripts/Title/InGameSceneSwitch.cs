2e38111c6c8bb5c0129033e687f238d809bc588d
陳　雨桑 <21cr0122@jec.ac.jp>
2022-10-06 11:40:36 +0900

    タイトルシーン追加

---
+++ Assets/Scripts/Title/InGameSceneSwitch.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameSceneSwitch : MonoBehaviour
+{
+    public float lateTime;
+    public  float timer;
+    public bool isCount;
+    void Start ()
+    {
+        isCount = false;
+        timer = 0;
+    }
+    
+    // Update is called once per frame
+    void Update () 
+    {
+        if( Input.GetKeyDown((KeyCode.Space)))
+        {
+            isCount = true;
+            
+        }
+
+        if (isCount)
+        {
+            timer += Time.deltaTime;
+        }
+        if (timer > lateTime)
+        {
+            SceneManager.LoadScene("SampleScene");
+            timer = 0;
+            isCount = false;
+        } 
+
+    }
+}
