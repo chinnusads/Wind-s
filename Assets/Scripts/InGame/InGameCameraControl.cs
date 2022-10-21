aecfcb0894d9be2ba6167544e1435a3eb4a33fdc
FangJihai <769673401@qq.com>
2022-10-21 15:50:44 +0900

    カメラ修正

--- Assets/Scripts/InGame/InGameCameraControl.cs
+++ Assets/Scripts/InGame/InGameCameraControl.cs
@@ -7,9 +7,15 @@ public class InGameCameraControl : MonoBehaviour
 {
     private float hight;
     public float states;
+    private Camera cam;
+    public float minPlayerHeight, maxPlayerHeight;
+    private float minCameraSize, maxCameraSize;
+
     void Start()
     {
-        
+        cam = GetComponent<Camera>();
+        minCameraSize = 4.6f;
+        maxCameraSize = 5.4f;
     }
 
     // Update is called once per frame
@@ -17,5 +23,10 @@ public class InGameCameraControl : MonoBehaviour
     {
         hight = PlayerInfo.playerY*states;
         this.transform.position = new Vector3(this.transform.position.x, hight, this.transform.position.z);
+        //0
+        //6.112197
+        //4.6
+        //5.4
+        cam.orthographicSize = (PlayerInfo.playerY/(maxPlayerHeight-minPlayerHeight) * (maxCameraSize -minCameraSize)) + minCameraSize;
     }
 }
