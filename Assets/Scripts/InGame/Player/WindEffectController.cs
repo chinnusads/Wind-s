77b153808673c33f3caea61275bc40637d4b8607
FangJihai <769673401@qq.com>
2022-10-21 15:03:56 +0900

    風エフェクト

--- Assets/Scripts/InGame/Player/WindEffectController.cs
+++ Assets/Scripts/InGame/Player/WindEffectController.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 public class WindEffectController : MonoBehaviour
 {
     public static Animator anim;
-    private bool animDisappear;
+    public float position1, position2, position3,position0;
+    public GameObject player;
+    private int position;
 
     void Start()
     {
         anim = GetComponent<Animator>();
-        animDisappear = false;
+        position = 0;
     }
 
 
@@ -34,6 +36,49 @@ public class WindEffectController : MonoBehaviour
         {
             anim.SetBool("NoCharge", false);
         }
+        switch (position)
+        {
+            case 0:
+                {
+                    this.gameObject.transform.position = new Vector3(this.transform.position.x, player.transform.position.y + position0, this.transform.position.z);
+                    break;
+                }
+            case 1:
+                {
+                    this.gameObject.transform.position = new Vector3(this.transform.position.x, player.transform.position.y + position1, this.transform.position.z);
+                    break;
+                }
+            case 2:
+                {
+                    this.gameObject.transform.position = new Vector3(this.transform.position.x, player.transform.position.y + position2, this.transform.position.z);
+                    break;
+                }
+            case 3:
+                {
+                    this.gameObject.transform.position = new Vector3(this.transform.position.x, player.transform.position.y + position3, this.transform.position.z);
+                    break;
+                }
+        }
+    }
+
+    void SetPosition0()
+    {
+        position = 0;
+    }
+
+    void SetPosition1()
+    {
+        position = 1;
+    }
+
+    void SetPosition2()
+    {
+        position = 2;
+    }
+
+    void SetPostion3()
+    {
+        position = 3;
     }
 
     void Disappear()
